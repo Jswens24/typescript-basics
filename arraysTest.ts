3dd652f79eac2f1826e1a9628ee8be46f023c3a2
Jessica Swenson <jessicamswens@gmail.com>
2023-04-26 07:51:49 -0600

    added arrays exercise

---
+++ arraysTest.ts
@@ -0,0 +1,23 @@
+const activeUsers: string[] = ['Jimmy']
+activeUsers.push('Bob')
+
+const ageList: number[] = [45, 56, 13]
+ageList[0] = 99
+
+
+const bools: Array<boolean> = []
+const boolsTwo: boolean[] = []
+
+type Point = {
+    x: number,
+    y: number
+}
+
+const coords: Point[] = []
+coords.push({ x: 23, y: 34 })
+
+
+const board: string[][] = [["X", "O", "X"], ["X", "O", "X"], ["X", "O", "X"]]
+
+const demo: number[][][] = [[[1]]]
+
